93020fd7cf635809a3b1d8d1c6cd1968fe194416
stpn94 <stpn94@gmail.com>
2021-10-07 14:30:06 +0900

    update

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,24 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("UTF-8");
+
+		// 개행
+		PrintWriter pw = response.getWriter();
+		pw.print("<h1>hellow world servlect</h1>");
+
+	}
+
+}
