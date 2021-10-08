996eb28d7835e6d3c92610a6e604f64c083c5800
stpn94 <stpn94@gmail.com>
2021-10-08 17:27:24 +0900

    mvc 2 emaillist02

---
+++ emaillist02/src/main/java/com/douzone/emaillist/dao/test/EmaillistDaoTest.java
@@ -0,0 +1,30 @@
+package com.douzone.emaillist.dao.test;
+
+import java.util.List;
+
+import com.douzone.emaillist.dao.EmaillistDao;
+import com.douzone.emaillist.vo.EmaillistVo;
+
+public class EmaillistDaoTest {
+
+	public static void main(String[] args) {
+		insertTest();
+		findAllTest();
+	}
+
+	private static void findAllTest() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		for (EmaillistVo vo : list) {
+			System.out.println(vo);
+		}
+	}
+
+	private static void insertTest() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName("또");
+		vo.setLastName("치");
+		vo.setEmail("ddochi@gmail.com");
+		new EmaillistDao().insert(vo);
+	}
+
+}
